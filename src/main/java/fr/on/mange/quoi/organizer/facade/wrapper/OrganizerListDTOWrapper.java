fc1648a3f18db3ad495674bcc4474c8537276f13
Mehdi <mehdibenmansour.pro@gmail.com>
2021-02-18 09:00:14 +0100

    [#10] List Organizers on main page with connected user

---
+++ src/main/java/fr/on/mange/quoi/organizer/facade/wrapper/OrganizerListDTOWrapper.java
@@ -0,0 +1,30 @@
+package fr.on.mange.quoi.organizer.facade.wrapper;
+
+import fr.on.mange.quoi.organizer.domain.model.Organizer;
+import fr.on.mange.quoi.organizer.facade.dto.OrganizerListDTO;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class OrganizerListDTOWrapper {
+    public List<OrganizerListDTO> fromModels(List<Organizer> models){
+        List<OrganizerListDTO> dtos = new ArrayList<>();
+        for(Organizer model: models){
+            dtos.add(fromModel(model));
+        }
+        return dtos;
+    }
+
+    public OrganizerListDTO fromModel(Organizer model){
+        OrganizerListDTO dto = new OrganizerListDTO();
+
+        if(model.getOptId().isPresent()) {
+            dto.setUuid(model.getOptId().get());
+        }
+        dto.setLabel(model.getLabel());
+
+        return dto;
+    }
+}
--- src/main/java/fr/on/mange/quoi/organizer/persistence/entity/OrganizerEntity.java
+++ src/main/java/fr/on/mange/quoi/organizer/persistence/entity/OrganizerEntity.java
@@ -1,5 +1,6 @@
 package fr.on.mange.quoi.organizer.persistence.entity;
 
+import fr.on.mange.quoi.user.persistance.UserEntity;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
@@ -16,13 +17,20 @@ public class OrganizerEntity {
     @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
 
-    @Column(name = "user_id")
     private String userId;
 
     private String label;
 
     @OneToMany(mappedBy = "organizer", fetch = FetchType.LAZY)
     private Set<DayOrganizerEntity> days = new HashSet<>();
+
+    public OrganizerEntity() {
+    }
+
+    public OrganizerEntity(String userId, String label) {
+        this.userId = userId;
+        this.label = label;
+    }
 
     public String getId() {
         return id;
