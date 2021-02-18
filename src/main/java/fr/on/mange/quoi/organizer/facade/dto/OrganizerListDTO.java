7d87afe0a59f224aefa3219eb6ba961207ef5ba3
Mehdi <mehdibenmansour.pro@gmail.com>
2021-02-18 09:00:14 +0100

    [#6] List Organizers on main page with connected user

---
+++ src/main/java/fr/on/mange/quoi/organizer/facade/dto/OrganizerListDTO.java
@@ -0,0 +1,22 @@
+package fr.on.mange.quoi.organizer.facade.dto;
+
+public class OrganizerListDTO {
+    private String uuid;
+    private String label;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+}
