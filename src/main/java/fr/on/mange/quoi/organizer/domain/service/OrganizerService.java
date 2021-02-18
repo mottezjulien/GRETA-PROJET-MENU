6749f8b9c6f58e4d5f8ae57a6e328114eebd1c05
Mehdi <mehdibenmansour.pro@gmail.com>
2021-02-18 09:00:14 +0100

    [#10]Added Organizer List per user
    [#10]Added Organizer List per user

--- src/main/java/fr/on/mange/quoi/organizer/domain/service/OrganizerService.java
+++ src/main/java/fr/on/mange/quoi/organizer/domain/service/OrganizerService.java
@@ -5,9 +5,11 @@ import fr.on.mange.quoi.generic.exception.ApplicationServiceException;
 import fr.on.mange.quoi.organizer.domain.model.Organizer;
 import fr.on.mange.quoi.organizer.persistence.entity.OrganizerEntity;
 import fr.on.mange.quoi.organizer.persistence.repository.OrganizerRepository;
+import fr.on.mange.quoi.user.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -46,4 +48,47 @@ public class OrganizerService {
         }
         throw new ApplicationServiceException("Any organisation found");
     }
+
+    public Organizer findByUserId(String userId) throws ApplicationServiceException {
+        Optional<OrganizerEntity> optEntity = repository.findByUserId(userId)
+                .stream()
+                .findFirst();
+
+        if(optEntity.isPresent()){
+            try {
+                return wrapper.fromEntity(optEntity.get());
+            } catch (ApplicationCommunicationException e) {
+                throw new ApplicationServiceException(e);
+            }
+        }
+        throw new ApplicationServiceException("Organizer not found");
+    }
+
+    public Organizer findByLabel(String label) throws ApplicationServiceException {
+        Optional<OrganizerEntity> optEntity = repository.findByLabel(label);
+
+        if(optEntity.isPresent()){
+            try {
+                return wrapper.fromEntity(optEntity.get());
+            } catch (ApplicationCommunicationException e) {
+                throw new ApplicationServiceException(e);
+            }
+        }
+        throw new ApplicationServiceException("Organizer not found");
+    }
+
+    public List<Organizer> findAllByUserId(String UserId) throws ApplicationServiceException {
+        Optional<List<OrganizerEntity>> optListEntity= repository.findAllByUserId(UserId);
+
+        if(optListEntity.isPresent()){
+            try {
+                return wrapper.fromEntities(optListEntity.get());
+            } catch (ApplicationCommunicationException e) {
+                throw new ApplicationServiceException(e);
+            }
+        }else{
+            throw new ApplicationServiceException("Organizer not found");
+        }
+    }
+
 }
